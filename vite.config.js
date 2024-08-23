1ddbc463714f67e3a8e4b2be218d99046dc39efa
Dhanushkabasnayaka99 <k9185.dhanuska@gmail.com>
2024-08-23 22:28:03 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+ 
+// });
+
+
 export default defineConfig({
   plugins: [react()],
- 
-});
+  base: "/Social-Media-Web-Application-React/",
+})
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0'
+//   }
+// })
